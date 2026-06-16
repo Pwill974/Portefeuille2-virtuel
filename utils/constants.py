fa5c6756c3dc72d6be09d58ab8871a52d793d32d
Pwill974 <pwill.974@outlook.fr>
2026-06-16 15:56:08 +0400

    Add UNIVERSE constant with stock market data

--- utils/constants.py
+++ utils/constants.py
@@ -3,3 +3,19 @@ ALLOCATION = {
  "MOMENTUM":35,
  "SATELLITE":15
 }
+UNIVERSE = {
+
+"DCAM.PA":"World",
+"PSP5.PA":"S&P500",
+"PUST.PA":"Nasdaq",
+"PAEEM.PA":"Emergents",
+
+"HO.PA":"Thales",
+"SU.PA":"Schneider",
+"AI.PA":"Air Liquide",
+"TTE.PA":"TotalEnergies",
+"AM.PA":"Dassault Aviation",
+"STM.PA":"STMicroelectronics",
+"SAN.PA":"Sanofi"
+
+}
