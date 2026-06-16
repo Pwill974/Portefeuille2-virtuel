3d80799e390c9a88e3a95facca07a893945b0436
Pwill974 <pwill.974@outlook.fr>
2026-06-16 16:30:49 +0400

    Update title and add initial sidebar state

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import streamlit as st
 st.set_page_config(
     page_title="Alpha Zen Pro",
     page_icon="📈",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="collapsed"
 )
 
-st.title("Alpha Zen Pro")
-st.write("Bienvenue")
+st.title("📈 Alpha Zen Pro")
+st.write("Tableau de bord de gestion PEA")
