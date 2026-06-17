02357e04587efaadfb4a341f4bc0b34b6cf03da2
Pwill974 <pwill.974@outlook.fr>
2026-06-17 06:02:13 +0400

    Update 04_Ordres.py

--- pages/04_Ordres.py
+++ pages/04_Ordres.py
@@ -1,13 +1,22 @@
 import streamlit as st
 
+from services.auth import require_authentication, show_logout_button
+
+
+require_authentication()
+show_logout_button()
+
+
 st.title("Ordres du mois")
 
-st.success("""
-Acheter :
+st.success(
+    """
+    Acheter :
 
-Thales 400 €
+    Thales : 400 €
 
-World 300 €
+    World : 300 €
 
-Nasdaq 300 €
-""")
+    Nasdaq : 300 €
+    """
+)
